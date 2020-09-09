4b7085649571576099c8a629bde58b463b71b67b
Zireael07 <zi@mail.com>
2020-09-09 11:12:41 +0200

    part 4: basic shadowcasting FOV implemented

--- dark.c
+++ dark.c
@@ -34,6 +34,7 @@ typedef int64_t		i64;
 // our own stuff starts here
 #include "ecs.c"
 #include "map.c"
+#include "fov.c"
 
 struct context {
     SDL_Window *window;
@@ -57,28 +58,30 @@ void draw_map(PT_Console *console){
   	{
      	for (y = 0; y < MAP_HEIGHT; y++)	
     	{
-			tile = map[x][y];
-			//tile = get_tile(x,y);
+			if (fovMap[x][y] > 0) {
+				tile = map[x][y];
+				//tile = get_tile(x,y);
 
-			switch (tile)
-			{
-				case tile_floor:
+				switch (tile)
 				{
-					glyph = '.';
-					break;
+					case tile_floor:
+					{
+						glyph = '.';
+						break;
+					}
+						
+					case tile_wall:
+					{
+						glyph = '#';
+						break;
+					}
+					default:
+					{
+						glyph = 'x';
+					}
 				}
-					
-				case tile_wall:
-				{
-					glyph = '#';
-					break;
-				}
-				default:
-				{
-					glyph = 'x';
-				}
+				PT_ConsolePutCharAt(console, glyph, x, y, 0xFFFFFFFF, 0x000000FF);
 			}
-			PT_ConsolePutCharAt(console, glyph, x, y, 0xFFFFFFFF, 0x000000FF);
 		}
 	}
 }
@@ -96,8 +99,10 @@ void render_screen(SDL_Renderer *renderer, SDL_Texture *screen, PT_Console *cons
 	for (u32 i = 1; i < MAX_GO; i++) {
 		if (renderableComps[i].objectId > 0) {
 			Position *p = (Position *)getComponentForGameObject(&gameObjects[i], COMP_POSITION);
-			PT_ConsolePutCharAt(console, renderableComps[i].glyph, p->x, p->y, 
+			if (fovMap[p->x][p->y] > 0) {
+				PT_ConsolePutCharAt(console, renderableComps[i].glyph, p->x, p->y, 
 								renderableComps[i].fgColor, renderableComps[i].bgColor);
+			}
 		}
 	}
 
@@ -138,7 +143,9 @@ bool canMove(Position pos) {
 void main_loop(void *context) {
 	struct context *ctx = (struct context *)context;
 	SDL_Event event;
-		
+	
+	bool recalculateFOV = false;
+
 	while (SDL_PollEvent(&event) != 0) {
 
 		if (event.type == SDL_QUIT) {
@@ -154,21 +161,39 @@ void main_loop(void *context) {
 
         if (key == SDLK_UP) {
 				Position newPos = {playerPos->objectId, playerPos->x, playerPos->y - 1};
-			 	if (canMove(newPos)) { addComponentToGameObject(ctx->player, COMP_POSITION, &newPos); } 
+			 	if (canMove(newPos)) { 
+					addComponentToGameObject(ctx->player, COMP_POSITION, &newPos);
+					recalculateFOV = true;
+				} 
 			}
         if (key == SDLK_DOWN) { 
 				Position newPos = {playerPos->objectId, playerPos->x, playerPos->y + 1};
-				if (canMove(newPos)) { addComponentToGameObject(ctx->player, COMP_POSITION, &newPos); } 
+				if (canMove(newPos)) { 
+					addComponentToGameObject(ctx->player, COMP_POSITION, &newPos);
+					recalculateFOV = true;
+				} 
 			}
         if (key == SDLK_LEFT) { 
 				Position newPos = {playerPos->objectId, playerPos->x - 1, playerPos->y};
-				if (canMove(newPos)) { addComponentToGameObject(ctx->player, COMP_POSITION, &newPos); } 
+				if (canMove(newPos)) { 
+					addComponentToGameObject(ctx->player, COMP_POSITION, &newPos);
+					recalculateFOV = true;
+				} 
 			}
         if (key == SDLK_RIGHT) { 
 				Position newPos = {playerPos->objectId, playerPos->x + 1, playerPos->y};
-				if (canMove(newPos)) { addComponentToGameObject(ctx->player, COMP_POSITION, &newPos); } 
+				if (canMove(newPos)) { 
+					addComponentToGameObject(ctx->player, COMP_POSITION, &newPos);
+					recalculateFOV = true;
+				} 
 			}
 
+	}
+
+	if (recalculateFOV) {
+		Position *pos = (Position *)getComponentForGameObject(ctx->player, COMP_POSITION);
+		fov_calculate(pos->x, pos->y, fovMap);
+		recalculateFOV = false;
 	}
 
 	render_screen(ctx->renderer, ctx->screen, ctx->console);
@@ -206,6 +231,9 @@ int main() {
 	addComponentToGameObject(player, COMP_PHYSICAL, &phys);
 
 	generate_map();
+
+	Position *playerPos = (Position *)getComponentForGameObject(player, COMP_POSITION);
+	fov_calculate(playerPos->x, playerPos->y, fovMap);
 
 	struct context ctx = {.window = window, .screen = screen, .renderer = renderer, .console = console, .player = player, .should_quit = false};
 
---
+++ fov.c
@@ -0,0 +1,185 @@
+/*
+* fov.c
+*/
+
+#define FOV_DISTANCE	5
+
+typedef struct {
+	u32 x, y;
+} FovCell;
+
+typedef struct {
+	float startSlope;
+	float endSlope;
+} Shadow;
+
+/* Function definitions (because functions have to be defined before use in C) */
+void add_shadow(Shadow s);
+bool cell_blocks_sight(u32 x, u32 y);
+bool cell_in_shadow(float cellSlope);
+float fov_distance_between(u32 x1, u32 y1, u32 x2, u32 y2);
+float line_slope_between(float x1, float y1, float x2, float y2);
+FovCell map_cell_for_local_cell(u8 sector, FovCell heroMapCell, FovCell cellToTranslate);
+
+global_variable u32 fovMap[MAP_WIDTH][MAP_HEIGHT];
+
+internal Shadow knownShadows[10];
+internal u8 shadowCount = 0;
+
+
+void fov_calculate(u32 heroX, u32 heroY, u32 fovMap[][MAP_HEIGHT]) {
+
+	// Reset FOV to default state (hidden)
+	for (u32 x = 0; x < MAP_WIDTH; x++) {
+		for (u32 y = 0; y < MAP_HEIGHT; y++) {
+			fovMap[x][y] = 0;
+		}
+	}
+
+	// Mark hero cell visible
+	fovMap[heroX][heroY] = 1;
+
+	// Loop through all 8 sectors around the player
+	for (u8 sector = 1; sector <= 8; sector++) {
+		bool prev_blocking = false;
+		float shadowStart = 0.0;
+		float shadowEnd = 0.0;
+		// For each distance from 1 to FOV range
+		for (int cellY = 1; cellY < FOV_DISTANCE; cellY++) {
+			prev_blocking = false;
+			// For each cell in the span
+			for (int cellX = 0; cellX <= cellY+1; cellX++) {
+				// Translate cellX, cellY to map coordinates
+				FovCell heroCell = {heroX, heroY};
+				FovCell cellToTranslate = {cellX, cellY};
+				FovCell mapCell = map_cell_for_local_cell(sector, heroCell, cellToTranslate);
+
+				// Is cell within map?
+				if ((mapCell.x >= 0) && (mapCell.y >= 0) && (mapCell.x < MAP_WIDTH) && (mapCell.y < MAP_HEIGHT)) {
+					// Is cell within view distance?
+					if (fov_distance_between(0, 0, cellX, cellY) <= FOV_DISTANCE) {
+						// Is cell within known shadow?
+						float cellSlope = line_slope_between(0, 0, cellX, cellY);
+						if (!cell_in_shadow(cellSlope)) {
+							// No - Mark as visible
+							fovMap[mapCell.x][mapCell.y] = 1;
+							// Is cell blocking?
+							if (cell_blocks_sight(mapCell.x, mapCell.y)) {
+								// Was the last cell blocking?
+								if (prev_blocking == false) {
+									// No - calc start of a new shadow
+									shadowStart = line_slope_between(0, 0, cellX-0.5, cellY);
+								}
+							} else {
+								// Was the last cell blocking?
+								if (prev_blocking) {
+									// Calc end slope of shadow.
+									shadowEnd = line_slope_between(0, 0, cellX+0.5, cellY);
+									// Add to shadow list 
+									Shadow s = {shadowStart, shadowEnd};
+									add_shadow(s);
+								}
+							}
+						}
+					}
+				}
+			}
+			// Do we have an open shadow
+			if (prev_blocking) {
+				// If so, calc end and add shadow to list before moving to next span
+				shadowEnd = line_slope_between(0, 0, cellY+1+0.5, cellY);
+				// Add to shadow list 
+				Shadow s = {shadowStart, shadowEnd};
+				add_shadow(s);
+			}
+		}
+	}
+
+}
+
+void add_shadow(Shadow s) {
+	knownShadows[shadowCount] = s;
+	shadowCount += 1;
+}
+
+bool cell_blocks_sight(u32 x, u32 y) {
+	// GameObject *go = game_object_at_position(x, y);
+	// if (go != NULL) {
+	// 	Physical *phys = (Physical *)game_object_get_component(go, COMP_PHYSICAL);
+	// 	if (phys->blocksSight) {
+	// 		return true;
+	// 	}
+	// }
+    if (map[x][y] == tile_wall) {
+        return true;
+    }
+	return false;
+}
+
+bool cell_in_shadow(float cellSlope) {
+	for (u8 i = 0; i < shadowCount; i++) {
+		Shadow s = knownShadows[i];
+		if (s.startSlope <= cellSlope && s.endSlope >= cellSlope) {
+			return true;
+		}
+	}
+	return false;
+}
+
+float fov_distance_between(u32 x1, u32 y1, u32 x2, u32 y2) {
+	return sqrt(pow(x2-x1, 2) + pow(y2-y1, 2));
+}
+
+float line_slope_between(float x1, float y1, float x2, float y2) {
+	return (y2 - y1) / (x2 - x1);
+}
+
+// Return the map cell corresponding to the given local sector coordinates
+FovCell map_cell_for_local_cell(u8 sector, FovCell heroMapCell, FovCell cellToTranslate) {
+	switch (sector) {
+		case 1: {
+			FovCell mapCell = {heroMapCell.x + cellToTranslate.x, heroMapCell.y - cellToTranslate.y};
+			return mapCell;
+		}
+		break;
+		case 2: {
+			FovCell mapCell = {heroMapCell.x + cellToTranslate.y, heroMapCell.y - cellToTranslate.x};
+			return mapCell;
+		}
+		break;
+		case 3: {
+			FovCell mapCell = {heroMapCell.x + cellToTranslate.y, heroMapCell.y + cellToTranslate.x};
+			return mapCell;
+		}
+		break;
+		case 4: {
+			FovCell mapCell = {heroMapCell.x + cellToTranslate.x, heroMapCell.y + cellToTranslate.y};
+			return mapCell;
+		}
+		break;
+		case 5: {
+			FovCell mapCell = {heroMapCell.x - cellToTranslate.x, heroMapCell.y + cellToTranslate.y};
+			return mapCell;
+		}
+		break;
+		case 6: {
+			FovCell mapCell = {heroMapCell.x - cellToTranslate.y, heroMapCell.y + cellToTranslate.x};
+			return mapCell;
+		}
+		break;
+		case 7: {
+			FovCell mapCell = {heroMapCell.x - cellToTranslate.y, heroMapCell.y - cellToTranslate.x};
+			return mapCell;
+		}
+		break;
+		case 8: {
+			FovCell mapCell = {heroMapCell.x - cellToTranslate.x, heroMapCell.y - cellToTranslate.y};
+			return mapCell;
+		}
+		break;
+		default:
+			assert(false);
+			FovCell c = {0,0};
+			return c;
+	}
+}
