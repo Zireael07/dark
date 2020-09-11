1b9862ffcd383bc4a7f413b278743deafbf6bfa6
Zireael07 <zi@mail.com>
2020-09-11 10:59:58 +0200

    new: basic immediate mode GUI/TUI implementation, groundwork for message log

---
+++ list.c
@@ -0,0 +1,227 @@
+/*
+* list.c
+*/
+
+typedef struct ListElement_ {
+	void *data;
+	struct ListElement_ *prev;
+	struct ListElement_ *next;
+} ListElement;
+
+typedef struct {
+	u32 size;
+
+	void (*destroy)(void *data);
+
+	ListElement *head;
+	ListElement *tail;
+} List;
+
+
+/* 
+Returns the number of elements in the given list
+*/
+#define list_size(list) ((list)->size)
+
+/*
+Get the header or tail of the given list
+*/ 
+#define list_head(list) ((list)->head)
+#define list_tail(list) ((list)->tail)
+
+/*
+Access the data for the given element
+*/
+#define list_data(element) ((element)->data)
+#define list_next(element) ((element)->next)
+#define list_prev(element) ((element)->prev)
+
+/*
+list_new creates a new List and must be called before the list can be used.
+Pass a pointer to a function that will clean up dynamically allocated
+memory used by the data contained in list elements. 
+e.g. for malloc, send a pointer to free. For data that shouldn't be cleaned
+up, send NULL.
+*/
+List * list_new(void (*destroy)(void* data)) {
+	List *list = malloc(sizeof(List));
+
+	if (list != NULL) {
+		list->size = 0;
+		list->destroy = destroy;
+		list->head = NULL;
+		list->tail = NULL; 
+	}
+
+	return list;
+}
+
+/*
+Inserts an element into the list after the given element.
+The new element contains a pointer to data, so the memory referenced
+by data should remain valid for the life of the element.
+Returns true on success, false on failure.
+*/
+bool list_insert_after(List *list, ListElement *element, void *data) {
+	ListElement *newElement;
+
+	if ((newElement = (ListElement *)malloc(sizeof(ListElement))) == NULL) {
+		return false;
+	}
+
+	newElement->data = (void *)data;
+
+	if (element == NULL) {
+		// Insert at head of list
+		if (list_size(list) == 0) {
+			list->tail = newElement;
+		} else {
+			list->head->prev = newElement;		
+		}
+
+		newElement->next = list->head;
+		newElement->prev = NULL;
+		list->head = newElement;
+
+	} else {
+		if (element->next == NULL) {
+			list->tail = newElement;
+		}
+
+		newElement->next = element->next;
+		newElement->prev = element;
+		element->next = newElement;
+	}
+
+	list->size += 1;
+	return true;
+}
+
+ListElement * list_item_at(List *list, u32 index) {
+	u32 currIdx = 0;
+	ListElement *currElement = list->head;
+
+	while (currElement != NULL)	{
+		if (currIdx == index) {
+			return currElement;
+		}
+		currIdx += 1;
+		currElement = currElement->next;
+	}
+
+	return NULL;
+}
+
+/*
+Removes the given element. If element is NULL, removes the head of the list. 
+Returns a pointer to the data stored in the element, or NULL on failure.
+*/
+void * list_remove(List *list, ListElement *element) {
+	ListElement *elementToRemove;
+	void *data = NULL;
+
+	if (list_size(list) == 0) {
+		return NULL;
+	}
+
+	if (element == NULL) {
+		data = list->head->data;
+		elementToRemove = list->head;
+		list->head = list->head->next;
+		if (list->head != NULL) {
+			list->head->prev = NULL;
+		}
+
+	} else {
+		data = element->data;
+		elementToRemove = element;
+
+		ListElement *prevElement = element->prev;
+		ListElement *nextElement = element->next;
+
+		if (prevElement != NULL && nextElement != NULL) {
+			prevElement->next = nextElement;
+			nextElement->prev = prevElement;
+
+		} else {
+			if (prevElement == NULL) {
+				// We're at start of list
+				if (nextElement != NULL) { nextElement->prev = NULL; }
+				list->head = nextElement;
+			} 
+			if (nextElement == NULL) {
+				// We're at end of list
+				if (prevElement != NULL) { prevElement->next = NULL; }
+				list->tail = prevElement;
+			}
+		}
+	}
+
+	free(elementToRemove);
+
+	list->size -= 1;
+
+	return data;
+}
+
+
+/*
+Removes the element containing the given data, if such an element is found. 
+*/
+void list_remove_element_with_data(List *list, void *data) {
+	if ((list_size(list) == 0) || (data == NULL)) {
+		return;
+	}
+
+	ListElement *elementToRemove = list->head;
+	while (elementToRemove != NULL) {
+		if (elementToRemove->data == data) {
+			break;
+		}
+
+		elementToRemove = elementToRemove->next;
+	}
+
+	if (elementToRemove != NULL) {
+		list_remove(list, elementToRemove);
+	}
+
+	return;
+}
+
+/* 
+Returns the element containing the given data. Returns NULL if no elements
+in the list contain the given data.
+*/
+ListElement * list_search(List *list, void * data) {
+	ListElement *currElement = list->head;
+
+	while (currElement != NULL)	{
+		if (currElement->data == data) {
+			return currElement;
+		}
+		currElement = currElement->next;
+	}
+
+	return NULL;
+}
+
+/*
+Removes all elements from the given list and calls the function 
+passed as destroy on the data in each element in the list.
+*/
+void list_destroy(List *list) {
+	// Remove each element
+	while (list_size(list) > 0) {
+		void *data = list_remove(list, NULL);
+		if (data != NULL && list->destroy != NULL) {
+			// Call the assigned function to free the dynamically allocated data
+			list->destroy(data);
+		}
+	}
+
+	// Free the list structure itself
+	free(list);
+}
+
+
---
+++ utils.c
@@ -0,0 +1,82 @@
+/*
+ * util.c - Miscellaneous utility fns
+ */
+
+
+/* Convert a character string representing a hex value to an int */
+int xtoi(char *hexstring) {
+	int	i = 0;
+	
+	if ((*hexstring == '0') && (*(hexstring+1) == 'x')) {
+		  hexstring += 2;
+	}
+
+	while (*hexstring) {
+		char c = toupper(*hexstring++);
+		if ((c < '0') || (c > 'F') || ((c > '9') && (c < 'A'))) {
+			break;
+		}
+		c -= '0';
+		if (c > 9) {
+			c -= 7;
+		}
+		i = (i << 4) + c;
+	}
+
+	return i;
+}
+
+
+/* String Manipulation Helpers *******************************************************************/
+
+//Because not every platform has asprintf built-in, and it's safer than sprintf because it allocates (what 'a' stands for...)
+#ifndef HAVE_ASPRINTF
+#include <stdio.h>  //vsnprintf
+#include <stdlib.h> //malloc
+#include <stdarg.h> //va_start et al
+
+int asprintf(char **str, char* fmt, ...){
+    va_list argp;
+    va_start(argp, fmt);
+    char one_char[1];
+    int len = vsnprintf(one_char, 1, fmt, argp);
+    if (len < 1){
+        fprintf(stderr, "An encoding error occurred. Setting the input pointer to NULL.\n");
+        *str = NULL;
+        return len;
+    }
+    va_end(argp);
+
+    *str = malloc(len+1);
+    if (!str) {
+        fprintf(stderr, "Couldn't allocate %i bytes.\n", len+1);
+        return -1;
+    }
+    va_start(argp, fmt);
+    vsnprintf(*str, len+1, fmt, argp);
+    va_end(argp);
+    return len;
+}
+#endif
+
+#ifdef Test_asprintf
+int main(){
+    char *s;
+    asprintf(&s, "hello, %s.", "—Reader—");
+    printf("%s\n", s);
+
+    asprintf(&s, "%c", '\0');
+    printf("blank string: [%s]\n", s);
+
+    int i = 0;
+    asprintf(&s, "%i", i++);
+    printf("Zero: %s\n", s);
+}
+#endif
+
+/* Macro for clean string extending with formatting */
+#define sasprintf(write_to, ...) {		\
+	char *temp = (write_to);			\
+	asprintf(&(write_to), __VA_ARGS__);	\
+	free(temp);							\
+}
