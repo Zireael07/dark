1b60e9dbf39a4d381a3adf1e4def1c68c57b3e48
Zireael07 <katarzynamosz@gmail.com>
2020-09-26 17:32:47 +0000

    Deploying to gh-pages from  @ 3279b48f4ccfae79f5850732634978760aac646d 🚀

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"files": [{"filename": "/assets/npcs.json", "start": 0, "end": 1835, "audio": 0}, {"filename": "/assets/terminal16x16.png", "start": 1835, "end": 10075, "audio": 0}], "remote_package_size": 10075, "package_uuid": "6aab9b8b-6c11-4b23-b514-6ab642ca7f49"});
+   loadPackage({"files": [{"filename": "/assets/npcs.json", "start": 0, "end": 1835, "audio": 0}, {"filename": "/assets/terminal16x16.png", "start": 1835, "end": 10075, "audio": 0}], "remote_package_size": 10075, "package_uuid": "6ba1d2b5-de6b-4d4b-9261-b69f0ddd8ae0"});
   
   })();
   
